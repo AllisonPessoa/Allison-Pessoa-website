4e7a27fc3d1336b6b027c74fcde1c16f4b9a1e65
AllisonPessoa <66957124+AllisonPessoa@users.noreply.github.com>
2022-07-15 13:47:45 -0300

    Organized repetitive code
    javascript is repeating the header, menu and footer through the files. Also: included research description

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,50 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+async function addHead() {
+  const resp = await fetch("views/head.html");
+  const html = await resp.text();
+  element = document.getElementById("head")
+  element.insertAdjacentHTML("afterbegin", html);
+}
+
+async function addFooter() {
+  const resp = await fetch("views/footer.html");
+  const html = await resp.text();
+  element = document.getElementById("footer")
+  element.insertAdjacentHTML("afterbegin", html);
+}
+
+async function addMenu() {
+  const resp = await fetch("views/menu.html");
+  const html = await resp.text();
+  element = document.getElementById("menu")
+  element.insertAdjacentHTML("afterbegin", html);
+
+  var pathname = window.location.pathname;
+  switch(pathname) {
+     case "/index.html" :
+         document.getElementById("menu_about").className = "nav-link active";
+         break;
+     case "/research.html" :
+         document.getElementById("menu_research").className = "nav-link active";
+         break;
+     case "/tech-instrumentation.html" :
+     case "/tech-softwares.html" :
+         document.getElementById("menu_technical").className = "nav-link dropdown-toggle active";
+         break;
+     case "/pub-banners.html" :
+     case "/pub-full.html" :
+     case "/pub-oral.html" :
+         document.getElementById("menu_pub").className = "nav-link dropdown-toggle active";
+         break;
+     case "/contact.html" :
+         document.getElementById("menu_contact").className = "nav-link active";
+         break;
+  }
+}
+
+addHead()
+addFooter()
+addMenu()
