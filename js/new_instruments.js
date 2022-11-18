c85223bc58baf1a721b19cd9816dc36bb1166210
AllisonPessoa <66957124+AllisonPessoa@users.noreply.github.com>
2022-11-18 08:27:00 +0100

    Added the link for TCSPC

--- js/new_instruments.js
+++ js/new_instruments.js
@@ -1,4 +1,4 @@
-function addOral(title, author, description, year, project_page, github_page, image) {
+function addInstrument(title, author, description, year, project_page, github_page, image) {
   document.getElementById("paper_container").innerHTML +=
     "<div class='card border-1 shadow rounded-3 overflow-hidden'>\
         <div class='card-body p-0'>\
@@ -20,7 +20,7 @@ function addOral(title, author, description, year, project_page, github_page, im
     </div>";
 }
 
-addOral("3D-printable Motorized Wheel for optical filter turning",
+addInstrument("3D-printable Motorized Wheel for optical filter turning",
 "Allison Pessoa",
 "Often in optics laboratories, one needs to filter the light before analyzing it. It can be done by using commercially available optical filters. This project is a 3D-printed wheel, attached to a stepper motor, where optical filters can be placed at it. The motor can rotate the wheel, thus changing the filter which is in the optical path. Two pushbuttons, placed away from the wheel, for example, control the filter in-line. Project used for Lab automatization by me at UFPE, Recife, Brazil.",
 "2022",
@@ -28,7 +28,7 @@ addOral("3D-printable Motorized Wheel for optical filter turning",
 "",
 "images/instruments-rotation_wheel.png");
 
-addOral("Heating Device",
+addInstrument("Heating Device",
 "Allison Pessoa",
 "The Heating Device acts to hold at a constant and adjustable setpoint (through PID control) the temperature in a thermal blanket (heater), while a thermistor is constantly reading the temperature. The communication PC-Heater is done via serial, through which some commands can be sent, like changing the PID parameters or changing the temperature setpoint value. Furthermore, a GUI (Graphic User Interface) can be designed to control the hardware, and plot the temperature in real-time. This project was shared with Simon Bongarz at LMU, Munich, Germany, for his Master's project.",
 "2020",
@@ -36,10 +36,10 @@ addOral("Heating Device",
 "https://github.com/AllisonPessoa/heating-device",
 "images/instruments-heating_device.png");
 
-addOral("TCSPC",
+addInstrument("TCSPC",
 "Allison Pessoa",
 "Time-resolved measurements of the luminescence intensity of a system allows us to study the photophysical dynamics of the problem. This project is a Time-Correlated Single Photon Counter (TCSPC) which counts pulses in very small time-intervals from a photon-counter. This project was used to measure the excited-state lifetimes of lanthanide-doped single nanoparticles. It was also used to perform magnetometry measurements in Nitrogen-vacancy defects in nanodiamonds. NOT AVAILABLE YET.",
 "2019",
 "",
-"",
+"https://github.com/AllisonPessoa/Arduino-TCSPC",
 "images/instrument-tcspc.png");
